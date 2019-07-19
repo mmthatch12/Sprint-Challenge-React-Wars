e945d53b4cd8475c0a4683205c6a6f30171c8fad
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-19 11:55:42 -0600

    MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -4,7 +4,7 @@ import CharacterGrid from './components/CharacterGrid'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  // the state properties here. I put my state stuff on CharacterGrid.js.
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
