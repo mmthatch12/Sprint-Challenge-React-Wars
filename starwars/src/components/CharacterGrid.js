26d3743bf37d13a4c8c0f11899663551f0654e83
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-19 09:28:38 -0600

    working on axios promise

--- starwars/src/components/CharacterGrid.js
+++ starwars/src/components/CharacterGrid.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
-
+//https://swapi.co/api/people/?format=json
 
 export default CharacterGrid () {
-    const
+    const [name, setName] = useState([])
+
+    useEffect(() => {
+        axios 
+    })
 }
